e588fb6fcd2e6f5f62cd26c8af472fb220745185
Diogo Lima <diogo.lima@grupoitss.com.br>
2022-03-07 14:15:23 -0300

    organizando em pastas

--- java/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteTributaveis.java
+++ java/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteTributaveis.java
@@ -1,4 +1,10 @@
+package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.CalculadorDeImposto;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.SeguroDeVida;
+
+//classe Teste
 public class TesteTributaveis {
 
 	public static void main(String[] args) {
