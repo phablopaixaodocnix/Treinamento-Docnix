11ec0cf18c6a10e2a9c64031410a1d5650a7a107
Guilhermebarrosf <guilhermebarrosf@hotmail.com>
2022-08-12 10:22:10 -0300

    Campo Numero Adicionado e Mascaras Validadas

--- Guilherme_Barros/Desafio-01/script.js
+++ Guilherme_Barros/Desafio-01/script.js
@@ -1,6 +1,12 @@
+// Validar Numero
+var validarNumero = "0123456789"
+let aux = 0
+
 //Contatos
+
 var contatosAlternativos = document.getElementById("contatosAlternativos")
 var contadorContatos = 0
+
 // Mensagem de Validação
 
 const validacaoNome = document.getElementById("validacao-nome")
@@ -8,6 +14,7 @@ const validacaoEmail = document.getElementById("validacao-email")
 const validacaoCPF = document.getElementById("validacao-cpf")
 const validacaoEmailContato = document.getElementById("validacaoEmailContato")
 const validacaoNomeContato = document.getElementById("validacaoNomeContato")
+const validacaoNumeroContato = document.getElementById("validacaoNumeroContato")
 
 // Formulario
 
@@ -21,13 +28,18 @@ const btnNovoContato = document.getElementById("btnNovoContato")
 Formulario.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    // Declarações
+    // Declarações Dados Principais
 
     var nomePrincipal = document.getElementById('nomePrincipal').value
     var emailPrincipal = document.getElementById("emailPrincipal").value
     var cpfPrincipal = document.getElementById("cpfPrincipal").value
+
+    //Declarações Contato Principal
     var emailContatoPrincipal = document.getElementById("emailContatoPrincipal").value
     var nomeContatoPrincipal = document.getElementById("nomeContatoPrincipal").value
+    var numeroContatoPrincipal = document.getElementById("numeroContatoPrincipal").value
+
+
 
 
     // Função Validar Nome
@@ -108,33 +120,28 @@ Formulario.addEventListener("submit", (e) => {
             validacaoCPF.style.display = "block"
             validacaoCPF.innerHTML = "Campo Obrigatório"
         }
-        else if (cpfPrincipal.length == 11) {
+        else
             // Calculando Primeiro Digito Verificador
             for (let i = 0; i < 9; i++) {
                 calc += cpfPrincipal[i] * contadorAux
                 contadorAux--
             }
+        calc = (calc * 10) % 11
+
+        // Se calc for igual a primeiro digito verificador, primeiro digito valido
+        if (calc >= 10) calc = 0
+        if (calc == cpfPrincipal[9]) {
+            //Calculando Segundo Digito Verificador
+            contadorAux = 11
+            calc = 0
+            for (let i = 0; i < 10; i++) {
+                calc += cpfPrincipal[i] * contadorAux
+                contadorAux--
+            }
             calc = (calc * 10) % 11
 
-            // Se calc for igual a primeiro digito verificador, primeiro digito valido
-            if (calc >= 10) calc = 0
-            if (calc == cpfPrincipal[9]) {
-                //Calculando Segundo Digito Verificador
-                contadorAux = 11
-                calc = 0
-                for (let i = 0; i < 10; i++) {
-                    calc += cpfPrincipal[i] * contadorAux
-                    contadorAux--
-                }
-                calc = (calc * 10) % 11
-
-                if (calc == cpfPrincipal[10]) {
-                    validacaoCPF.style.display = "none"
-                }
-                else {
-                    validacaoCPF.style.display = "block"
-                    validacaoCPF.innerHTML = "CPF Incorreto"
-                }
+            if (calc == cpfPrincipal[10]) {
+                validacaoCPF.style.display = "none"
             }
             else {
                 validacaoCPF.style.display = "block"
@@ -149,7 +156,15 @@ Formulario.addEventListener("submit", (e) => {
 
     // Função Validar Numero
 
-    let regexNumero = [(][0-9]{2}[)][9][0-9]{4}[-][0-9]{4}
+    let validarNumero = ()=>{
+        if(numeroContatoPrincipal == ""){
+            validacaoNumeroContato.style.display = "block"
+            validacaoNumeroContato.innerHTML = "Campo Obrigatório"
+        }
+        else{
+            validacaoNumeroContato.style.display = "none"
+        }
+    }
 
 
     // Funções Globais
@@ -158,11 +173,13 @@ Formulario.addEventListener("submit", (e) => {
         validarNome()
         validarEmail()
         validarCPF()
+
     }
 
     let validacaoContatoPrincipal = () => {
         validarNome()
         validarEmail()
+        validarNumero()
     }
 
     validacaoDadosPrincipais()
@@ -182,7 +199,7 @@ let criarContato = () => {
     contatosAlternativos.appendChild(novoContatoTitulo)
     contadorContatos++
 
-    //Input Name
+    //Input Nome
 
     let novoContatoNome = document.createElement('input')
     novoContatoNome.setAttribute("type", "text")
@@ -204,3 +221,11 @@ let criarContato = () => {
     contatosAlternativos.appendChild(novoContatoNumero)
 
 }
+
+// Mascara para campos
+
+$(document).ready(function () {
+    $('#numeroContatoPrincipal').mask('(00) 00000-0000');
+    $('#maskCep').mask('00000-00')
+    $('#cpfPrincipal').mask('000.000.000-00')
+});
